7785d746fd2cd7b649e2c7c1f60cc9ad1e864e27
Hanna <anutalahmaniuk@mail.com>
2024-10-03 22:50:13 +0300

    first commit Hanna

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('.form');
+const input = document.querySelector('.form__input');
+
+input.addEventListener('focus', function (){
+    form.classList.add('form--active')
+})
+
+input.addEventListener('blur', function (){
+    form.classList.remove('form--active')
+})
+
+
+function confirmCall(number) {
+    const confirmMessage = `Ви впевнені, що хочете зателефонувати на номер ${number}?`;
+    if (confirm(confirmMessage)) {
+        window.location.href = `tel:${number}`;
+    }
+}
