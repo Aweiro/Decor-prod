3add53ba55bf27b71f8b73a5eb093b146ca6c88f
Hanna <anutalahmaniuk@mail.com>
2024-10-08 22:37:41 +0300

    add hamburger and carousel

--- js/script.js
+++ js/script.js
@@ -37,3 +37,39 @@ function topFunction() {
 
 
 //hamburger menu
+document.getElementById('hamburgerBtn').addEventListener('click', function() {
+  const navigationList = document.querySelector('.navigation-list');
+  navigationList.classList.toggle('show'); 
+});
+
+// carusel
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
+
+// Автоматичне перемикання слайдів кожні 3 секунд
+setInterval(() => {
+    changeSlide(1);
+}, 3000);
+
+
