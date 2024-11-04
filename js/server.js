add6cca91b21b0b796f8db97aa574caa35e2a98b
Arsenii Bohak <arsenskott@gmail.com>
2024-11-04 21:14:17 +0200

    try second day p10

--- js/server.js
+++ js/server.js
@@ -1,62 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
-
 const path = require('path');
 const cors = require('cors');
-const fs = require('fs');
-const db = require('./firebase');
-
-const bodyParser = require('body-parser');
-// const upload = multer({ dest: 'uploads/' });
-const upload = multer({ storage: multer.memoryStorage() });
-
+const db = require('./firebase'); // підключення до Firestore
+const { v4: uuidv4 } = require('uuid'); // UUID для унікального токена
+const { Storage } = require('@google-cloud/storage'); // для роботи з Firebase Storage
 
 const app = express();
-// const PORT = process.env.PORT || 3000;
+const upload = multer({ storage: multer.memoryStorage() }); // зберігання у пам'яті
+
+// Firebase Storage
+const storage = new Storage();
+const bucket = storage.bucket('west-decor.appspot.com'); // Замість цього додайте вашу назву bucket в .env
 
 // Налаштування CORS
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Додаємо для обробки form-data
+app.use(express.urlencoded({ extended: true })); // для обробки form-data
 
 // Налаштування статичних файлів
 app.use('/css', express.static(path.join(__dirname, '../css')));
 app.use('/img', express.static(path.join(__dirname, '../img')));
 app.use('/js', express.static(path.join(__dirname, '../js')));
-
-// Перевірка та створення директорії uploads
-// const uploadDir = path.join(__dirname, 'uploads');
-// if (!fs.existsSync(uploadDir)) {
-//     fs.mkdirSync(uploadDir, { recursive: true });
-// }
-
-// Налаштування для зберігання файлів
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, uploadDir);
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, Date.now() + path.extname(file.originalname));
-//     },
-// });
-
-// const upload = multer({ storage: storage });
-
-// Функція для читання photos.json
-// const readPhotosJson = () => {
-//     const filePath = path.join(__dirname, 'photos.json');
-//     if (!fs.existsSync(filePath)) {
-//         fs.writeFileSync(filePath, JSON.stringify([])); // Створити файл, якщо його немає
-//     }
-//     const data = fs.readFileSync(filePath);
-//     return JSON.parse(data);
-// };
-
-// Функція для запису у photos.json
-// const writePhotosJson = (photos) => {
-//     const filePath = path.join(__dirname, 'photos.json');
-//     fs.writeFileSync(filePath, JSON.stringify(photos, null, 2));
-// };
 
 // Маршрути для відображення HTML-сторінок
 app.get('/', (req, res) => {
@@ -71,33 +37,6 @@ app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, '../admin.html'));
 });
 
-// Обробка запиту на завантаження фото
-// app.post('/upload', upload.single('photo'), (req, res) => {
-//     console.log("Запит на завантаження фото отримано"); // Лог для підтвердження запиту
-//     console.log("Дані форми:", req.body);
-//     console.log("Файл:", req.file);
-
-//     const { description, decorName, price } = req.body;
-//     if (req.file) {
-//         const photos = readPhotosJson();
-//         const newPhoto = {
-//             name: req.file.filename,
-//             url: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`,
-//             description: description || 'Опис відсутній',
-//             decorName: decorName || 'Назва декору відсутня',
-//             price: price ? parseFloat(price) : 0
-//         };
-//         photos.push(newPhoto);
-//         writePhotosJson(photos);
-        
-//         console.log("Фото успішно збережено:", newPhoto); // Лог для підтвердження збереження
-//         res.status(200).json({ message: 'Фото успішно завантажено!', file: newPhoto });
-//     } else {
-//         console.log("Фото не завантажено"); // Лог для випадку, коли файл не завантажено
-//         res.status(400).json({ message: 'Не вдалося завантажити фото.' });
-//     }
-// });
-
 // Отримання списку фото
 app.get('/photos', async (req, res) => {
     try {
@@ -110,61 +49,57 @@ app.get('/photos', async (req, res) => {
     }
 });
 
-
 // Додавання нового фото
 app.post('/upload', upload.single('photo'), (req, res) => {
-	const { description, decorName, price } = req.body;
-	
-	if (req.file) {
-			console.log('Файл завантажено:', req.file); // Додаємо лог для перевірки
+    const { description, decorName, price } = req.body;
 
-			const uniqueToken = uuidv4();
-			const blob = bucket.file(`uploads/${req.file.originalname}`);
-			const blobStream = blob.createWriteStream({
-					metadata: {
-							contentType: req.file.mimetype,
-							metadata: {
-									firebaseStorageDownloadTokens: uniqueToken
-							}
-					}
-			});
+    if (req.file) {
+        console.log('Файл завантажено:', req.file); // Лог для перевірки
 
-			blobStream.on('error', (error) => {
-					console.error("Помилка завантаження в Firebase Storage:", error);
-					res.status(500).json({ message: 'Не вдалося завантажити фото.' });
-			});
+        const uniqueToken = uuidv4();
+        const blob = bucket.file(`uploads/${req.file.originalname}`);
+        const blobStream = blob.createWriteStream({
+            metadata: {
+                contentType: req.file.mimetype,
+                metadata: {
+                    firebaseStorageDownloadTokens: uniqueToken
+                }
+            }
+        });
 
-			blobStream.on('finish', async () => {
-					const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/uploads%2F${encodeURIComponent(req.file.originalname)}?alt=media&token=${uniqueToken}`;
+        blobStream.on('error', (error) => {
+            console.error("Помилка завантаження в Firebase Storage:", error);
+            res.status(500).json({ message: 'Не вдалося завантажити фото.' });
+        });
 
-					const newPhoto = {
-							name: req.file.originalname,
-							url: publicUrl,
-							description: description || 'Опис відсутній',
-							decorName: decorName || 'Назва декору відсутня',
-							price: price ? parseFloat(price) : 0
-					};
+        blobStream.on('finish', async () => {
+            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/uploads%2F${encodeURIComponent(req.file.originalname)}?alt=media&token=${uniqueToken}`;
 
-					try {
-							await db.collection('photos').add(newPhoto);
-							console.log("Фото успішно збережено:", newPhoto);
-							res.status(200).json({ message: 'Фото успішно завантажено!', file: newPhoto });
-					} catch (error) {
-							console.error("Помилка збереження в Firestore:", error);
-							res.status(500).json({ message: 'Не вдалося зберегти фото в Firestore.' });
-					}
-			});
+            const newPhoto = {
+                name: req.file.originalname,
+                url: publicUrl,
+                description: description || 'Опис відсутній',
+                decorName: decorName || 'Назва декору відсутня',
+                price: price ? parseFloat(price) : 0
+            };
 
-			blobStream.end(req.file.buffer);
-	} else {
-			res.status(400).json({ message: 'Файл не завантажено.' });
-	}
+            try {
+                await db.collection('photos').add(newPhoto);
+                console.log("Фото успішно збережено:", newPhoto);
+                res.status(200).json({ message: 'Фото успішно завантажено!', file: newPhoto });
+            } catch (error) {
+                console.error("Помилка збереження в Firestore:", error);
+                res.status(500).json({ message: 'Не вдалося зберегти фото в Firestore.' });
+            }
+        });
+
+        blobStream.end(req.file.buffer);
+    } else {
+        res.status(400).json({ message: 'Файл не завантажено.' });
+    }
 });
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
