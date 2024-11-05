2cd0e50c438f92b1c878fa85d74890460241ad27
Arsenii Bohak <arsenskott@gmail.com>
2024-11-05 13:03:28 +0200

    test upload 3

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
 const admin = require('firebase-admin');
+const { v4: uuidv4 } = require('uuid');
 
+const app = express();
+
+// Ініціалізація Firebase Admin SDK
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
@@ -10,8 +16,13 @@ const serviceAccount = {
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "west-decor.appspot.com"
+  storageBucket: 'west-decor.appspot.com' // замініть на ваш Firebase Storage bucket
 });
 
 const db = admin.firestore();
-module.exports = db;
+const bucket = admin.storage().bucket();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const upload = multer({ storage: multer.memoryStorage() });
