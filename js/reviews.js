ef943731a73eaabb221a5644fc9c7a959193707d
Hanna <anutalahmaniuk@mail.com>
2024-11-10 21:54:36 +0200

    reviews 3

--- js/reviews.js
+++ js/reviews.js
@@ -1,7 +1,7 @@
 // Ініціалізація Firebase для клієнтської сторони
 const firebaseConfig = {
   type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
