e4dcee4cc876de948fe0b84dbc9381388bba151a
Hanna <anutalahmaniuk@mail.com>
2024-11-17 20:33:04 +0200

    hash pass bcrypt 1

--- js/server.js
+++ js/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
